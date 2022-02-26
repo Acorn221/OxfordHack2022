b52170276cdc84a1a6d2c420fde90ddd67dee976
Danny Piper <djpiper28@gmail.com>
2022-02-26 16:22:23 +0000

    added /rand

--- friends/main.go
+++ friends/main.go
@@ -7,6 +7,7 @@ import (
   "os"
   "github.com/joho/godotenv"
   "database/sql"
+  "encoding/json"
   _ "github.com/lib/pq"
 )
 
@@ -18,6 +19,11 @@ type Config struct {
   dbport string
 }
 
+type Post struct {
+  id string
+}
+
+var conf = Config {};
 func getconn(config Config) (*sql.DB, error) {
   psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
     config.dburl,
@@ -45,9 +51,58 @@ func statusHandler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintln(w, "running");
 }
 
+func getrandomposts(w http.ResponseWriter, r *http.Request) {
+  log.Println("Returning random posts");
+
+	// Connect and select
+	pq, err := getconn(conf);
+	if (err != nil) {
+    fmt.Fprintln(w, "{\"error\":\"cannot fetch random posts\"");
+    log.Println(err);
+    return;
+	}
+  defer pq.Close();
+
+	rows, err := pq.Query("select id from post limit 25;");
+	if (err != nil) {
+    fmt.Fprintln(w, "{\"error\":\"cannot fetch random posts\"");
+    log.Println(err);
+    return;
+	}
+
+	defer rows.Close();
+
+	// Get from table
+	var posts []Post;
+  for rows.Next() {
+	  var post_id string;
+		err := rows.Scan(&post_id);
+
+		if (err != nil) {
+      fmt.Fprintln(w, "{\"error\":\"cannot fetch random posts\"");
+      log.Println(err);
+      return;
+  	}
+
+		_ = append(posts, Post{id: post_id});
+	}
+
+	// To json
+  json, err := json.Marshal(posts);
+	if (err != nil) {
+    fmt.Fprintln(w, "{\"error\":\"cannot fetch random posts\"");
+    log.Println(err);
+    return;
+	}
+
+  fmt.Fprintln(w, string(json));
+}
+
 func main() {
   log.SetFlags(2 | 3);
 	log.Println("Starting the friends server...");
+
+	// Get conf
 	godotenv.Load();
 	dbname := getenv("DB_NAME");
 	dbusername := getenv("DB_USERNAME");
@@ -61,8 +116,10 @@ func main() {
 	  dbpassword: dbpassword,
 		dburl: dburl,
 		dbport: dbport};
+  conf = config;
 	log.Printf("Database configuration: %s.\n", config);
 
+	// Test config
   pq, err := getconn(config);
   if (err != nil) {
     log.Fatalf("Unable to connect to the database %s.\n");
@@ -75,6 +132,7 @@ func main() {
 
   // Setup and start the server
   http.HandleFunc("/", statusHandler);
+  http.HandleFunc("/rand", getrandomposts);
   log.Fatal(http.ListenAndServe(bindaddr, nil));
 }
 
