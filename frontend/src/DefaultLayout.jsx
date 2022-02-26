5654d6d7c150bf6809fdc463b3f50f330b4dac17
John Costa <johncosta027@gmail.com>
2022-02-26 19:54:46 +0000

    Changed display font.

--- frontend/src/DefaultLayout.jsx
+++ frontend/src/DefaultLayout.jsx
@@ -9,17 +9,17 @@ const DefaultLayout = () => {
       <div className="w-full h-16 border-b-2 border-slate-200 flex justify-between items-center">
         <div className="w-full h-full flex justify-center items-center">
           <Link to="/">
-            <p className="text-md text-white">Home</p>
+            <p className="text-xl text-white">Home</p>
           </Link>
         </div>
         <div className="w-full h-full flex justify-center items-center">
           <Link to="/profile">
-            <p className="text-md text-white">Profile</p>
+            <p className="text-xl text-white">Profile</p>
           </Link>
         </div>
         <div className="w-full h-full flex justify-center items-center">
           <Link to="/upload">
-            <p className="text-md text-white">Upload</p>
+            <p className="text-xl text-white">Upload</p>
           </Link>
         </div>
         <div className="w-full h-full flex justify-center items-center">
--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -4,33 +4,43 @@ const Profile = () => {
   return (
     <div className="w-full flex flex-col">
       <div className="w-full h-96 bg-red-500"></div>
-      <div className="-my-16 z-10 w-full h-36 flex justify-center">
+      <div className="-my-16 z-10 w-full h-96 flex justify-center">
         <div className="w-full h-full flex flex-col justify-center items-center">
           <img
             className="aspect-square h-full rounded-full"
             src="https://thumbs.dreamstime.com/z/selfie-times-square-young-man-taking-44840581.jpg"
           />
-          <h1 className="text-3xl text-white">John Costa</h1>
+          <h1 className="text-7xl text-white">John Costa</h1>
         </div>
       </div>
-      <div className="mt-32 w-full grid grid-cols-2 gap-2 p-2">
+      <div className="my-32 w-full px-4 flex flex-col justify-center items-center gap-16">
         <Picture
+          subject="Danny"
+          photographer="John"
           caption="Caption #1"
           pictureUrl="https://thumbs.dreamstime.com/z/selfie-times-square-young-man-taking-44840581.jpg"
         />
         <Picture
+          subject="Danny"
+          photographer="John"
           caption="Caption #1"
           pictureUrl="https://thumbs.dreamstime.com/z/selfie-times-square-young-man-taking-44840581.jpg"
         />
         <Picture
+          subject="Danny"
+          photographer="John"
           caption="Caption #1"
           pictureUrl="https://thumbs.dreamstime.com/z/selfie-times-square-young-man-taking-44840581.jpg"
         />
         <Picture
+          subject="Danny"
+          photographer="John"
           caption="Caption #1"
           pictureUrl="https://thumbs.dreamstime.com/z/selfie-times-square-young-man-taking-44840581.jpg"
         />
         <Picture
+          subject="Danny"
+          photographer="John"
           caption="Caption #1"
           pictureUrl="https://thumbs.dreamstime.com/z/selfie-times-square-young-man-taking-44840581.jpg"
         />
