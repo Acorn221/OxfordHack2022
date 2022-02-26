40db37924d1d9543c9ac9a7cd957b98224c1bfea
James <jamesarnott2@gmail.com>
2022-02-26 20:25:49 +0000

    yeet

--- frontend/src/Upload.jsx
+++ frontend/src/Upload.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Dropzone from './DropArea'
 
 const Upload = () => {
   return (
-    <div className="w-full">
+    <div className="w-full h-full">
       <h1 className="text-2xl text-white">Upload image: </h1>
+      <Dropzone className="h-full"/>
     </div>
   );
 };
