c57e208941d362eace6b3bc9fd6ca7b7b74a201a
James <jamesarnott2@gmail.com>
2022-02-26 20:58:37 +0000

    yooo

--- frontend/src/DropArea.jsx
+++ frontend/src/DropArea.jsx
@@ -7,12 +7,12 @@ const DropArea = () => {
   const [fileNames, setFileNames] = useState([]);
   const handleDrop = (files) => {
     console.log(files);
-    var formData = new FormData();
-    formData.append('file', files[0]);
+    //var formData = new FormData();
+    //formData.append('file', files[0]);
 
     fetch(uploadURL, {
       method: 'POST',
-      body: formData,
+      body: files[0],
       headers: {
         'img-id': uuidv4(),
       },
@@ -21,13 +21,13 @@ const DropArea = () => {
   return (
     <Dropzone onDrop={handleDrop}>
       {({ getRootProps, getInputProps }) => (
-        <div {...getRootProps({ className: 'px-64 dropzone h-3/4' })}>
+        <div {...getRootProps({ className: 'px-64 dropzone h-3/4 hover:shadow-l hover:wiggle shadow-white drop-shadow-xl' })}>
           <input {...getInputProps()} />
           <p className="text-black text-center ring-offset-red-700 bg-white margin place-content-center h-4/5 flex items-center">
             Drop your image here or click here to upload it!
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-16 w-16 align-center m-5"
+              className="h-16 w-16 align-center m-5 animate-bounce"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor">
