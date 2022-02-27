74418e4145c0a5cdf6ec887ecab1ce22ac0d6444
John Costa <johncosta027@gmail.com>
2022-02-27 04:43:42 +0000

    Adding server information to the picture feed.

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Picture from './Picture';
 
 import { useAuth0 } from '@auth0/auth0-react';
@@ -8,47 +8,34 @@ import axios from 'axios';
 const Home = () => {
   const { user } = useAuth0();
 
+  const [feed, setFeed] = useState([]);
+
   useEffect(() => {
     axios.get('http://localhost/api/getfeed').then((response) => {
-      console.log(response);
+      setFeed(response.data);
     });
   }, []);
+
+  const getPeopleInPicture = (usersArray) => {
+    let subjects = '';
+    for (let user of usersArray) {
+      subjects += user + ' ';
+    }
+    return subjects;
+  };
 
   return (
     <div className="w-full grid grid-cols-12">
       <div className="col-span-10 w-full flex flex-col justify-center items-center">
         <h1 className="text-3xl text-white">Recent Posts</h1>
         <div className="w-full px-4 flex flex-col justify-center items-center gap-16">
-          <Picture
-            subject="Danny"
-            photographer="John"
-            caption="Caption #1"
-            pictureUrl="https://thumbs.dreamstime.com/z/selfie-times-square-young-man-taking-44840581.jpg"
-          />
-          <Picture
-            subject="Danny"
-            photographer="John"
-            caption="Caption #1"
-            pictureUrl="https://thumbs.dreamstime.com/z/selfie-times-square-young-man-taking-44840581.jpg"
-          />
-          <Picture
-            caption="Caption #1"
-            subject="Danny"
-            photographer="John"
-            pictureUrl="https://thumbs.dreamstime.com/z/selfie-times-square-young-man-taking-44840581.jpg"
-          />
-          <Picture
-            subject="Danny"
-            photographer="John"
-            caption="Caption #1"
-            pictureUrl="https://thumbs.dreamstime.com/z/selfie-times-square-young-man-taking-44840581.jpg"
-          />
-          <Picture
-            subject="Danny"
-            photographer="John"
-            caption="Caption #1"
-            pictureUrl="https://thumbs.dreamstime.com/z/selfie-times-square-young-man-taking-44840581.jpg"
-          />
+          {feed.map((item) => (
+            <Picture
+              key={item['Image_id']}
+              photographer={item['User_name']}
+              subject={getPeopleInPicture(item['In_image'])}
+            />
+          ))}
         </div>
       </div>
       <div className="col-span-2 h-full "></div>
