ce94e509391ba64233312a230c4fa2f3cc472bb5
John Costa <johncosta027@gmail.com>
2022-02-27 05:34:20 +0000

    If employers are reading this, please stop its 5am.

--- frontend/src/Upload.jsx
+++ frontend/src/Upload.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dropzone from './DropArea';
 
 const Upload = () => {
+  const [userId, setUserId] = useState('');
+
   return (
     <div className="w-full h-screen">
+      <input type="text" label="Userid" onChange={(e) => setUserId(e.target.value)} />
       <h1 className="text-2xl text-white">Upload image: </h1>
-      <Dropzone />
+      <Dropzone userId={userId} />
     </div>
   );
 };
